b704f94cc4ae03642b869edf570383e45a7810ff
Bertrand Janin <b@janin.com>
2014-01-30 22:57:58 -0500

    Implement join_list() for later use.

---
+++ tests/regress/str/stub.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <strings.h>
+
+#include "str.h"
+#include "utils.h"
+
+
+static int
+join_list_wrapper(char **av)
+{
+	const char sep = av[2][0];
+	int count = atoi(av[3]);
+	char *output;
+
+	output = join_list(sep, count, av + 4);
+
+	printf("%s\n", output);
+
+	return EXIT_SUCCESS;
+}
+
+
+static int
+join_wrapper(char **av)
+{
+	const char sep = av[2][0];
+	char *base = av[3];
+	char *suffix = av[4];
+	char *output;
+
+	output = join(sep, base, suffix);
+
+	printf("%s\n", output);
+
+	return EXIT_SUCCESS;
+}
+
+
+int
+main(int ac, char **av)
+{
+	(void)(ac);
+
+	if (strcmp(av[1], "join") == 0) {
+		return join_wrapper(av);
+	} else if (strcmp(av[1], "join_list") == 0) {
+		return join_list_wrapper(av);
+	} else {
+		return EXIT_FAILURE;
+	}
+}
--- tests/regress/utils/stub.c
+++ tests/regress/utils/stub.c
@@ -8,22 +8,6 @@
 
 
 static int
-join_wrapper(char **av)
-{
-	const char sep = av[2][0];
-	const char *base = av[3];
-	const char *suffix = av[4];
-	char *output;
-
-	output = join(sep, base, suffix);
-
-	printf("%s\n", output);
-
-	return EXIT_SUCCESS;
-}
-
-
-static int
 join_path_wrapper(char **av)
 {
 	const char *base = av[2];
@@ -56,9 +40,7 @@ main(int ac, char **av)
 {
 	(void)(ac);
 
-	if (strcmp(av[1], "join") == 0) {
-		return join_wrapper(av);
-	} else if (strcmp(av[1], "join_path") == 0) {
+	if (strcmp(av[1], "join_path") == 0) {
 		return join_path_wrapper(av);
 	} else if (strcmp(av[1], "file_exists") == 0) {
 		return file_exists_wrapper(av);
