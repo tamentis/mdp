01e874871ea3d1b98bb7848d4e01a1d11bc8bbb3
Bertrand Janin <b@janin.com>
2014-02-03 17:51:53 -0500

    Properly create initial password for pager timeout test.

--- tests/functional/test_pager_timeout.sh
+++ tests/functional/test_pager_timeout.sh
@@ -1,7 +1,10 @@
 # Filter on two keywords, test timeout setting (should pause for 3 seconds).
 
+# Create initial passwords.
 use_config simple
+run_mdp edit
 
+# Time the pager when not given any input.
 start_ts=`date +%s`
 run_mdp get red > /dev/null
 stop_ts=`date +%s`
